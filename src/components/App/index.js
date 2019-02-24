28898e35e2a7a671d85144e0a03dc2de49c190bf
Boris Hasikov <laapl@yandex.ru>
2019-02-24 16:08:32 +0800

    Подключение глобальных стилей

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,19 +1,29 @@
 import React from "react";
 import { Router } from "react-router-dom";
 import { Provider } from "react-redux";
+import { ThemeProvider } from "styled-components";
 import store, { history } from "../../store";
 // CC
 import Content from "../Content";
 import SideBar from "../SideBar";
+// UI
+import { GlobalStyle } from "./@ui";
+
+const theme = {
+  color: "black",
+};
 
 const App = () => {
   return (
     <Provider store={store}>
       <Router history={history}>
-        <>
-          <Content />
-          <SideBar />
-        </>
+        <ThemeProvider theme={theme}>
+          <>
+            <GlobalStyle />
+            <Content />
+            <SideBar />
+          </>
+        </ThemeProvider>
       </Router>
     </Provider>
   );
